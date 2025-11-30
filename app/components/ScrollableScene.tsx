9ccb8d74f7046069a6481081a33d0eb8eeecdf7c
jtpotato <58995538+jtpotato@users.noreply.github.com>
2025-11-30 22:32:31 +1100

    mobile warning

--- app/components/ScrollableScene.tsx
+++ app/components/ScrollableScene.tsx
@@ -9,8 +9,11 @@ export function ScrollableScene() {
   const scale = 10;
 
   return (
-    <div ref={ref} style={{ height: `${scale * 100}vh` }}>
-      <div className="h-screen fixed top-0 left-0 w-screen bg-black">
+    <div ref={ref} style={{ height: `${scale * 100}vh` }} className="bg-black">
+      <div className="md:h-screen fixed top-1/3 md:top-0 left-0 w-screen aspect-video md:aspect-auto">
+        <p className="md:hidden text-center text-xs">
+          Experience best viewed in landscape.
+        </p>
         <SceneCanvas percentage={percentage} scale={scale} />
       </div>
     </div>
